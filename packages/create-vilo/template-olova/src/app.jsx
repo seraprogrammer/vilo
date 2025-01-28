fe63eedc01690cefa21a29306ac5eb35556293e1
seraprogrammer <programmernazmul1@gmail.com>
2025-01-28 07:42:30 +0600

    new update v2

--- packages/create-vilo/template-olova/src/app.jsx
+++ packages/create-vilo/template-olova/src/app.jsx
@@ -1,31 +1,40 @@
-import Olova, { $signal, $memo } from "olova";
+import { $state } from "olova";
 import "./style.css";
+import Logo from "/olova.png";
 
 const App = () => {
-  const [count, setCount] = $signal(0);
-  const result = $memo(() => count * 2, [count]);
-
+  const [count, setCount] = $state(0);
   return (
     <>
-      <div className="app-container">
-        <main className="main-content">
-          <div className="hero-section">
-            <h1 className="title">
-              Welcome to <span className="highlight">Olova</span>
-            </h1>
-            <p className="subtitle">The Bangladeshi Framework</p>
+      <div className="gradient-bg bg-1"></div>
+      <div className="gradient-bg bg-2"></div>
 
-            <div className="counter-section">
-              <button
-                className="primary-button"
-                onClick={() => setCount(count + 1)}
-              >
-                Count: {count}
-              </button>
-              <p className="counter-value">Double value: {result}</p>
-            </div>
-          </div>
-        </main>
+      <div className="container">
+        <div className="logo">
+          <a href="https://github.com/olovajs/olova">
+            <img src={Logo} alt="" />
+          </a>
+        </div>
+        <h1>Vite + Olova</h1>
+        <p>
+          Click on the Olova logo to learn more about framework, or edit
+          <code>src/App.jsx</code> and save to test updates.
+        </p>
+
+        <div className="count-btn">
+          <button className="button" onClick={() => setCount(count + 1)}>
+            count is {count}
+          </button>
+        </div>
+
+        <div className="buttons">
+          <a href="https://olova.js.org/" className="button primary">
+            Get Started
+          </a>
+          <a href="https://olova.js.org/tryolova.html" className="button">
+            Documentation
+          </a>
+        </div>
       </div>
     </>
   );
--- packages/create-vilo/template-olova/src/main.jsx
+++ packages/create-vilo/template-olova/src/main.jsx
@@ -1,5 +1,4 @@
 import Olova from "olova";
-
 import App from "./app.jsx";
-const root = document.getElementById("root");
-Olova.render(App, root);
+
+Olova.mount(App, document.getElementById("root"));
