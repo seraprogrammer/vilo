5ce18c83342c65cb9567bc8a7d1dbe2885d75f3a
Sera Programmer <73139993+seraprogrammer@users.noreply.github.com>
2025-03-10 08:53:46 +0600

    Update app.jsx

--- packages/create-vilo/template-olova/src/app.jsx
+++ packages/create-vilo/template-olova/src/app.jsx
@@ -1,42 +1,53 @@
-import { setSignal } from "olova";
+import { html, Fragment, setSignal, setEffect } from "olova";
 import "./style.css";
 import Logo from "/olova.png";
 
 const App = () => {
   const [count, setCount] = setSignal(0);
+
+  setEffect(() => {
+    console.log("app is rendered");
+  });
+
+  setEffect(() => {
+    console.log(count());
+  });
+
   return (
-    <div>
-      <div className="gradient-bg bg-1"></div>
-      <div className="gradient-bg bg-2"></div>
+    <>
+      <div>
+        <div className="gradient-bg bg-1"></div>
+        <div className="gradient-bg bg-2"></div>
 
-      <div className="container">
-        <div className="logo">
-          <a href="https://github.com/olovajs/olova">
-            <img src={Logo} alt="" />
-          </a>
-        </div>
-        <h1>Vite + Olova</h1>
-        <p>
-          Click on the Olova logo to learn more about framework, or edit
-          <code>src/App.jsx</code> and save to test updates.
-        </p>
+        <div className="container">
+          <div className="logo">
+            <a href="https://github.com/olovajs/olova">
+              <img src={Logo} alt="" />
+            </a>
+          </div>
+          <h1>Vite + Olova</h1>
+          <p>
+            Click on the Olova logo to learn more about framework, or edit
+            <code>src/App.jsx</code> and save to test updates.
+          </p>
 
-        <div className="count-btn">
-          <button className="button" onClick={() => setCount((c) => c + 1)}>
-            count is {count}
-          </button>
-        </div>
+          <div className="count-btn">
+            <button className="button" onClick={() => setCount((c) => c + 1)}>
+              count is {() => count()}
+            </button>
+          </div>
 
-        <div className="buttons">
-          <a href="https://olova.js.org/" className="button primary">
-            Get Started
-          </a>
-          <a href="https://olova.js.org/learn.html" className="button">
-            Documentation
-          </a>
+          <div className="buttons">
+            <a href="https://olova.js.org/" className="button primary">
+              Get Started
+            </a>
+            <a href="https://olova.js.org/learn.html" className="button">
+              Documentation
+            </a>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
